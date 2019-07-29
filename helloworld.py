a820f62b69a00877f8d1ce2b1c8e22e7cc6482fe
HAuslander <jhrgovc@yahoo.com>
2019-07-29 10:20:35 -0500

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import optparse
+import copy
+
+import numpy as np
+
+import os.path
+import datetime, time
+
+import collections
+
+
+sys.path.append('/Users/joe/quant/')
+
+import terminal_openclose
+
+
+
+import glob
+
+
+
+
+def ProcessFile(opts=None):
+
+
+  print "HELLO"
+
+
+
+
+  
+
+
+if __name__ == '__main__':   
+  ProcessFile()     
+
+
+                                                                                                                  
+
+
+
